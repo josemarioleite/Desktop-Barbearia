68763dd68f76361f938976630b9def9628384f8e
Mário Leite <jose.leite@vibetecnologia.com>
2021-08-05 09:25:43 -0400

    Colocando enum origem

---
+++ Barbearia/Enum/OrigemEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace Barbersoft.Enum
+{
+    public enum OrigemEnum
+    {
+        Atendimento = 1,
+        ContaReceber = 2,
+        ContaPagar = 3,
+        Recebimento = 4,
+        Pagamento = 5
+    }
+}
